dfbab0cc12eddd43272c524b82d4eeeb4ec5e630
Tzu Huang Yen <tzuhuangyen@Tzus-MacBook-Pro.local>
2023-06-03 22:19:54 +0200

    review cards

--- all.js
+++ all.js
@@ -35,25 +35,28 @@ $(document).ready(function() {
 
 
 //index-reviewer
-// let swiper = new Swiper(".mySwiper", {
-//   slidesPerView: 1,
-//   spaceBetween: 24,
-//   pagination: {
-//     el: ".swiper-pagination",
-//     clickable: true,
-//   },
-//   breakpoints: {
-//     768: {
-//       slidesPerView: 1,
-//       // spaceBetween: 10,
-//     },
-//     1200: {
-//       slidesPerView: 3,
-//       // spaceBetween: 20,
-//     },
+let swiper = new Swiper(".mySwiper", {
+  spaceBetween: 24,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+
+    768: {
+      slidesPerView: 1,
+      // spaceBetween: 10,
+    },
+    992:{
+      slidesPerView: 2,
+
+    },
+    1240:{
+      slidesPerView:3,
+    }
    
-//   },
-// });
+  },
+});
 
 //index page
 $(document).ready(function() {
